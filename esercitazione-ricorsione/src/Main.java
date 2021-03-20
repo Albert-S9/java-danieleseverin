dfb608654dd61b6d18bb4326d8839da044acf892
Daniele Severin <danieleseverin89@gmail.com>
2021-03-20 11:35:29 +0100

    fatto fino all'esercizio 6

--- esercitazione-ricorsione/src/Main.java
+++ esercitazione-ricorsione/src/Main.java
@@ -28,7 +28,7 @@ public class Main {
         }
     }
 
-    //Scrivere un metodo ricorsivo che conti il numero di cifre dispari in un numero.
+    //3. Scrivere un metodo ricorsivo che conti il numero di cifre dispari in un numero.
     public static int contaDispari (int n){
         if(n / 10 == 0){
             if(n % 2 != 0)
@@ -43,6 +43,40 @@ public class Main {
                 return contaDispari(n/10);
         }
     }
+
+    //4. Scrivere un metodo ricorsivo che calcoli la somma delle cifre di un numero positivo.
+    public static int sommaCifre (int n) {
+        if (n /10 == 0)
+            return n;
+        else
+            return (n%10) + sommaCifre(n/10);
+    }
+
+    //5. Completare la definizione ricorsiva del metodo seguente:
+    /*
+    Precondizione: n >= 0
+    Restituisce 10 elevato alla potenza n.
+    */
+    public static int calcolaDieciAlla(int n){
+        if (n==0)
+            return 1;
+        else
+            return 10 * calcolaDieciAlla(n-1);
+    }
+
+    //6. Scrivere un metodo ricorsivo che calcoli la somma di tutti gli elementi di un array.
+    public static int sommaElementiArray(int[] array, int n){
+        if(n==0)
+            return array[n];
+        else
+            return array[n] + sommaElementiArray(array, n-1);
+    }
+
+    /*7. Scrivere un metodo ricorsivo che trovi e restituisca il valore più grande in un array di interi.
+    Suggerimento: dividere Tarray a metà e cercare rìcorsivamentc il valore massimo in ogni metà.
+    Restituire il maggiore tra i due valori.
+     */
+    
     
     public static void main(String[] args) {
 
@@ -60,5 +94,20 @@ public class Main {
         System.out.println("");
 
         System.out.println("Conta dispari: " + contaDispari(1357902441));
+
+        System.out.println("Somma Cifre: " + sommaCifre(12345));
+
+        System.out.println("calcolaDieciAlla: " + calcolaDieciAlla(4));
+
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        int n = array.length-1;
+
+        System.out.println("sommaElementiArray: " + sommaElementiArray(array, n));
+
     }
 }
