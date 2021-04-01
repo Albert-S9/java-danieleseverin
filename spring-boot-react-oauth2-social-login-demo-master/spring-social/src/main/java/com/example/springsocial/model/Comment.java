4d94daca5390356e4d5eb76e6785537d3c433931
Daniele Severin <danieleseverin89@gmail.com>
2021-04-01 11:26:29 +0200

    photo, photolike, comment

---
+++ spring-boot-react-oauth2-social-login-demo-master/spring-social/src/main/java/com/example/springsocial/model/Comment.java
@@ -0,0 +1,11 @@
+package com.example.springsocial.model;
+
+import java.util.Date;
+
+public class Comment {
+
+    private User owner;
+    private Photo photo;
+    private String text;
+    private Date date;
+}
---
+++ spring-boot-react-oauth2-social-login-demo-master/spring-social/src/main/java/com/example/springsocial/model/PhotoLike.java
@@ -0,0 +1,7 @@
+package com.example.springsocial.model;
+
+public class PhotoLike {
+
+    private User owner;
+    private Photo photo;
+}
