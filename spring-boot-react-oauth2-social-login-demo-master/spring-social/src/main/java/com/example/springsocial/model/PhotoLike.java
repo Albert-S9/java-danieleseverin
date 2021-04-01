674e02420d7ff28a0ce12f3353f8132e8e9166e4
Daniele Severin <danieleseverin89@gmail.com>
2021-04-01 13:27:10 +0200

    fine lezione

--- spring-boot-react-oauth2-social-login-demo-master/spring-social/src/main/java/com/example/springsocial/model/PhotoLike.java
+++ spring-boot-react-oauth2-social-login-demo-master/spring-social/src/main/java/com/example/springsocial/model/PhotoLike.java
@@ -1,7 +1,44 @@
 package com.example.springsocial.model;
 
+import javax.persistence.*;
+
+@Entity
+//@JsonidentityInfo( generator = ObjectIdgenerators.PropertyGenerator.class, property="id")
 public class PhotoLike {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="owner", referencedColumnName="id")
     private User owner;
+
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="photo", referencedColumnName="id")
     private Photo photo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
 }
